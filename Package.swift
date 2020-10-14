b8da047ed03e08ea407698377b3f884d77145e4d
Sascha Schwabbauer <sascha.schwabbauer@img.ly>
2020-10-14 18:39:17 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -5,8 +5,10 @@ let package = Package(
   name: "VideoEditorSDK",
   platforms: [.iOS(.v9)],
   products: [
-    .library(name: "ImglyKit", targets: ["ImglyKit"]),
-    .library(name: "VideoEditorSDK", targets: ["VideoEditorSDK"])
+    .library(name: "VideoEditorSDK", targets: ["VideoEditorSDK"]),
+  ],
+  dependencies: [
+    .package(name: "ImglyKit", url: "https://github.com/sascha/imglykit-spm-test.git", .branch("main"))
   ],
   targets: [
     .target(
@@ -14,10 +16,6 @@ let package = Package(
       dependencies: ["ImglyKit"],
       path: "Source",
       publicHeadersPath: ""
-    ),
-    .binaryTarget(
-      name: "ImglyKit", 
-      url: "https://www.dropbox.com/s/7fvdo7spoog10ef/ImglyKit.zip?dl=1", 
-      checksum: "fa27818ae388237261ac2e0bfd278f2bf10482f3b23969e3776f5c7ec2e283cd")
+    )
   ]
 )
