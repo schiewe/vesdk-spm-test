c0595818f1a66e1adba5e1c96ce82509d01250ee
Alexander Schiewe <alexander.schiewe@img.ly>
2022-05-12 15:23:50 +0200

    Add version 11.2.0-beta.2

--- Package.swift
+++ Package.swift
@@ -3,12 +3,16 @@ import PackageDescription
 
 let package = Package(
   name: "VideoEditorSDK",
-  platforms: [.iOS(.v13)],
+  platforms: [
+    .iOS(.v13),
+    .macCatalyst(.v13),
+    .macOS(.v10_15),
+  ],
   products: [
     .library(name: "VideoEditorSDK", targets: ["VideoEditorSDK"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/imgly/imglykit-sp.git", exact: "11.2.0-beta.1")
+    .package(url: "https://github.com/imgly/imglykit-sp.git", exact: "11.2.0-beta.2")
   ],
   targets: [
     .target(
